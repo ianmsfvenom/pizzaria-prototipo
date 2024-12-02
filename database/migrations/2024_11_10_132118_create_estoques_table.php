de34a580306c31da984a299afd808be2bb8df144
ianmsfvenom <iangamer296@gmail.com>
2024-12-02 19:44:01 -0300

    remove migration

--- database/migrations/2024_11_10_132118_create_estoques_table.php
+++ database/migrations/2024_11_10_132118_create_estoques_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->string('material');
+            $table->string('unidade_medida');
             $table->integer('quantidade');
         });
     }
